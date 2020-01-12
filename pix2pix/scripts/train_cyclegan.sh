e2cfa1a551e3d40c6b56b37b74800d9b60e43047
Shivam Khare <skhare31@fermat.cc.gatech.edu>
2020-01-12 12:25:22 -0800

    Making identity loss zero

--- pix2pix/scripts/train_cyclegan.sh
+++ pix2pix/scripts/train_cyclegan.sh
@@ -1,2 +1,2 @@
 set -ex
-python train.py --dataroot /data/skhare31/edge_birds_cleaned/ --name sobel_rgb_low --model cycle_gan_notext --niter 500 --niter_decay 100
+python train.py --continue_train --dataroot /data/skhare31/edge_birds_cleaned/ --name sobel_rgb_low --model datagan --niter 500 --niter_decay 100
